f93b0af1db4bd53ca0648389c1b77ba75b0456ff
Gauthier Bastien <g.bastien@imio.be>
2013-07-05 13:58:21 +0000

    Create necessary contacts directory

--- src/imio/project/core/setuphandlers.py
+++ src/imio/project/core/setuphandlers.py
@@ -7,5 +7,29 @@ def isNotCurrentProfile(context):
 
 def post_install(context):
     """Post install script"""
-    if isNotCurrentProfile(context): return
-    portal = context.getSite()
+    import ipdb; ipdb.set_trace()
+    if isNotCurrentProfile(context):
+        return
+
+    # add contacts
+    _addContacts(context)
+
+
+def _addContacts(context):
+    """Add needed organizations"""
+    if isNotCurrentProfile(context):
+        return
+    site = context.getSite()
+    # add the 'contacts' directory if it does not already exists
+    if not hasattr(site, 'contacts'):
+        organization_types = [{'name': u'Commune', 'token': 'commune'}, ]
+
+        organization_levels = [{'name': u'Echevinat', 'token': 'echevinat'},
+                               {'name': u'Service', 'token': 'service'}, ]
+
+        params = {'title': "Contacts",
+                  'position_types': [],
+                  'organization_types': organization_types,
+                  'organization_levels': organization_levels,
+                  }
+        site.invokeFactory('directory', 'contacts', **params)
