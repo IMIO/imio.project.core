df3dc1f898319bd93cf1cdd14252d81de38d3bdc
Stephan Geulette <s.geulette@imio.be>
2013-07-05 11:57:23 +0000

    added projectspace type #6023

---
+++ src/imio/project/core/content/projectspace.py
@@ -0,0 +1,71 @@
+from zope import schema
+from zope.interface import Interface, implements
+from z3c.form.widget import FieldWidget
+from plone.autoform import directives
+from plone.dexterity.content import Container
+from plone.dexterity.schema import DexteritySchemaPolicy
+from plone.supermodel import model
+from collective.z3cform.datagridfield import DataGridField, DictRow
+from imio.project.core import _
+
+
+def DataGridFieldWithListingTableFactory(field, request):
+    """
+        IFieldWidget factory for DataGridField using the 'listing' class for the main table.
+    """
+    widget = DataGridField(request)
+    # temporary fallback until display_table_css_class attribute is in collective.z3cform.datagridfield
+    if hasattr(widget, 'display_table_css_class'):
+        widget.display_table_css_class = 'listing'
+    return FieldWidget(field, widget)
+
+
+class IVocabularySchema(Interface):
+    """
+        Schema used for the vocabulary datagrid field.
+    """
+    label = schema.TextLine(
+        title=_("Label"),
+        required=True,)
+    key = schema.TextLine(
+        title=_("Key"),
+        required=True,)
+
+
+class IProjectSpace(model.Schema):
+    """
+        Project schema, field ordering
+    """
+    categories = schema.List(
+        title=_(u'Categories values'),
+        description=_(u"Enter one category by row. Label is the displayed value. Key is the stored value:"
+                      " in lowercase, without space."),
+        required=True,
+        value_type=DictRow(title=_("Categories"),
+                           schema=IVocabularySchema,
+                           required=False),
+    )
+    directives.widget(categories=DataGridFieldWithListingTableFactory)
+
+    priority = schema.List(
+        title=_(u'Priority values'),
+        description=_(u"Enter one priority by row. Label is the displayed value. Key is the stored value:"
+                      " in lowercase, without space."),
+        required=True,
+        value_type=DictRow(title=_("Priority"),
+                           schema=IVocabularySchema,
+                           required=False),
+    )
+    directives.widget(priority=DataGridFieldWithListingTableFactory)
+
+
+class ProjectSpace(Container):
+    """ """
+    implements(IProjectSpace)
+
+
+class ProjectSpaceSchemaPolicy(DexteritySchemaPolicy):
+    """ """
+
+    def bases(self, schemaName, tree):
+        return (IProjectSpace, )
