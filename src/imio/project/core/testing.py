8ac24b802487e80e8cfc21f42e4b6c2b5aa6e839
Gauthier Bastien <g.bastien@imio.be>
2013-07-09 08:45:44 +0000

    Setup tests

--- src/imio/project/core/testing.py
+++ src/imio/project/core/testing.py
@@ -1,79 +1,87 @@
 # -*- coding: utf-8 -*-
 """Base module for unittesting."""
 
-from plone.app.testing import applyProfile
+import unittest2
+from plone.app.testing import PloneWithPackageLayer
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import login
-from plone.app.testing import PLONE_FIXTURE
-from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import TEST_USER_NAME
-from plone.testing import z2
 
-import unittest2 as unittest
+from plone.dexterity.utils import createContentInContainer
 
 import imio.project.core
 
 
-class ImioProjectLayer(PloneSandboxLayer):
+PROJECT_TESTING_PROFILE = PloneWithPackageLayer(
+    zcml_filename="testing.zcml",
+    zcml_package=imio.project.core,
+    additional_z2_products=('imio.project.core', ),
+    gs_profile_id='imio.project.core:testing',
+    name="PROJECT_TESTS_PROFILE")
 
-    defaultBases = (PLONE_FIXTURE,)
+PROJECT_TESTING_PROFILE_INTEGRATION = IntegrationTesting(
+    bases=(PROJECT_TESTING_PROFILE,), name="PROJECT_TESTING_PROFILE_INTEGRATION")
 
-    def setUpZope(self, app, configurationContext):
-        """Set up Zope."""
-        # Load ZCML
-        self.loadZCML(package=imio.project.core,
-                      name='testing.zcml')
-        z2.installProduct(app, 'imio.project.core')
-
-    def setUpPloneSite(self, portal):
-        """Set up Plone."""
-        # Install into Plone site using portal_setup
-        applyProfile(portal, 'imio.project.core:default')
-
-        # Login and create some test content
-        setRoles(portal, TEST_USER_ID, ['Manager'])
-        login(portal, TEST_USER_NAME)
-        portal.invokeFactory('Folder', 'folder')
-
-        # Commit so that the test browser sees these objects
-        portal.portal_catalog.clearFindAndRebuild()
-        import transaction
-        transaction.commit()
-
-    def tearDownZope(self, app):
-        """Tear down Zope."""
-        z2.uninstallProduct(app, 'imio.project.core')
+PROJECT_TEST_PROFILE_FUNCTIONAL = FunctionalTesting(
+    bases=(PROJECT_TESTING_PROFILE,), name="PROJECT_TESTING_PROFILE_FUNCTIONAL")
 
 
-FIXTURE = ImioProjectLayer(
-    name="FIXTURE"
-    )
-
-INTEGRATION = IntegrationTesting(
-    bases=(FIXTURE,),
-    name="INTEGRATION"
-    )
-
-FUNCTIONAL = FunctionalTesting(
-    bases=(FIXTURE,),
-    name="FUNCTIONAL"
-    )
-
-
-class IntegrationTestCase(unittest.TestCase):
+class IntegrationTestCase(unittest2.TestCase):
     """Base class for integration tests."""
 
-    layer = INTEGRATION
+    layer = PROJECT_TESTING_PROFILE_INTEGRATION
 
     def setUp(self):
         super(IntegrationTestCase, self).setUp()
         self.portal = self.layer['portal']
 
 
-class FunctionalTestCase(unittest.TestCase):
+class FunctionalTestCase(unittest2.TestCase):
     """Base class for functional tests."""
 
-    layer = FUNCTIONAL
+    layer = PROJECT_TEST_PROFILE_FUNCTIONAL
+
+    def setUp(self):
+        super(FunctionalTestCase, self).setUp()
+        self.portal = self.layer['portal']
+        # login as Manager and add a projectspace and 2 projects into it
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        login(self.portal, TEST_USER_NAME)
+        params = {'id': u"projectspace",
+                  'title': u"projectspace"}
+        # datagridfield categories
+        categories = [
+            {'label': u"Category 1",
+             'key': u"category-1"},
+            {'label': u"Category 2",
+             'key': u"category-2"},
+            {'label': u"Category 3",
+             'key': u"category-3"},
+        ]
+        params['categories'] = categories
+        # datagridfield priority
+        priority = [
+            {'label': u"High",
+             'key': u"high"},
+            {'label': u"Normal",
+             'key': u"normal"},
+            {'label': u"Low",
+             'key': u"low"},
+        ]
+        params['priority'] = priority
+        projectspace = createContentInContainer(self.portal, 'projectspace', **params)
+        projects = [
+            {'id': u"project1",
+             'title': u"Project 1",
+             'categories': u"category-1",
+             'priority': u"high", },
+            {'id': u"project2",
+             'title': u"Project 2",
+             'categories': u"category-2",
+             'priority': u"normal", },
+        ]
+        for project in projects:
+            createContentInContainer(projectspace, 'project', **project)
