d7911b0b6fc975258b4b671c2e9bc468587afdf0
vpiret <vpiret@localhost>
2019-09-02 11:02:20 +0000

    Fix behavior budget

--- src/imio/project/core/browser/behaviors.py
+++ src/imio/project/core/browser/behaviors.py
@@ -2,112 +2,62 @@
 
 from collective.z3cform.datagridfield import DictRow
 from imio.project.core import _
-from imio.project.core.browser.widgets import BudgetInfosDataGridField
-from imio.project.core.utils import getProjectSpace
-from plone.app.textfield import RichText
 from plone.autoform import directives
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.dexterity.interfaces import IDexterityContent
 from plone.supermodel import model
-from z3c.form import interfaces
-from z3c.form.widget import FieldWidget
 from zope import schema
 from zope.component import adapter
 from zope.interface import implementer
 from zope.interface import Interface
 from zope.interface import provider
-from zope.schema.interfaces import IContextAwareDefaultFactory
-
-import datetime
-import zope.interface
+from collective.z3cform.datagridfield.datagridfield import DataGridField
+from imio.project.core.content.project import default_year
 
 
-@zope.interface.implementer(interfaces.IFieldWidget)
-def BudgetInfosDataGridFieldFactory(field, request):
-    """IFieldWidget factory for DataGridField using the 'BudgetInfosDataGridField' widget."""
-    widget = BudgetInfosDataGridField(request)
-    return FieldWidget(field, widget)
+class IAnalyticBudgetSchema(Interface):
+    """Schema used for the datagrid field 'analytic_budget' of IProject"""
 
-
-@provider(IContextAwareDefaultFactory)
-def default_year(context):
-    """
-      defaultFactory for the field IBudgetSchema.year
-    """
-    years = getProjectSpace(None).budget_years or []
-    year = datetime.date.today().year
-    if year in years:
-        return year
-    elif year + 1 in years:
-        return year + 1
-    elif years:
-        return years[-1]
-    else:
-        return None
-
-
-class IBudgetSchema(Interface):
-    """Schema used for the datagrid field 'budget' of IProject."""
-
-    budget_type = schema.Choice(
-        title=_(u"Budget_type"),
-        description=_(u"Choose a budget type."),
-        vocabulary=u"imio.project.core.content.project.budget_type_vocabulary",
-    )
     year = schema.Choice(
         title=_(u"Year"),
         description=_(u"Choose a year."),
         vocabulary=u"imio.project.core.content.project.year_vocabulary",
+        required=True,
         defaultFactory=default_year,
     )
+    economic_group = schema.TextLine(
+        title=_(u"Economic Group"),
+        description=_(u"Choose an economic group."),
+        required=True,
+    )
     amount = schema.Float(title=_("Amount"), required=True, default=0.0)
+    comment = schema.TextLine(
+        title=_(u"Comment"),
+        description=_(u"Write a comment about this budget line."),
+        required=False,
+    )
 
 
 @provider(IFormFieldProvider)
-class IBudget(model.Schema):
+class IAnalyticBudget(model.Schema):
     """Budget field"""
 
-    budget = schema.List(
-        title=_(u"Budget"),
-        description=_(
-            u"Enter budget details.  If you have comments about budget, "
-            "use the field here above."
-        ),
+    analytic_budget = schema.List(
+        title=_(u"Analytic Budget"),
         required=False,
-        value_type=DictRow(title=_("Budget"), schema=IBudgetSchema, required=False),
+        value_type=DictRow(
+            title=_("Analytic budget"), schema=IAnalyticBudgetSchema, required=False
+        ),
     )
     directives.widget(
-        "budget",
-        BudgetInfosDataGridFieldFactory,
+        "analytic_budget",
+        DataGridField,
         display_table_css_class="listing nosort",
     )
 
-    budget_comments = RichText(
-        title=_(u"Budget comments"),
-        description=_(u"Write here comments you have about budget."),
-        required=False,
-        default_mime_type="text/html",
-        output_mime_type="text/html",
-        allowed_mime_types=("text/html",),
-    )
 
-
-@implementer(IBudget)
+@implementer(IAnalyticBudget)
 @adapter(IDexterityContent)
-class Budget(object):
+class AnalyticBudget(object):
     def __init__(self, context):
         self.context = context
-
-    @property
-    def budget(self):
-        return set(self.context.Subject())
-
-    @budget.setter
-    def tags(self, value):
-        if value is None:
-            value = ()
-        self.context.setSubject(tuple(value))
-
-
-class IBudgetMarker(Interface):
-    """"""
