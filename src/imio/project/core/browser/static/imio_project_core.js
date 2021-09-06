0ec64c6e069e1363f42274d0893e079d02d25c8b
fngaha <franck.o.ngaha@gmail.com>
2021-09-06 12:09:27 +0200

    Used by default the JS function `setoddeven` of `helpers.js` PRJ-453

--- src/imio/project/core/browser/static/imio_project_core.js
+++ src/imio/project/core/browser/static/imio_project_core.js
@@ -2,12 +2,3 @@ $(document).ready(function(){
     /* by default, table with 'listing nosort' are not oddevened... */
     $('table.listing tbody').each(setoddeven);
 });
-
-/* helper function that set odd/ven class on rows of a table having the 'listing' class */
-function setoddeven() {
-    var tbody = $(this);
-    // jquery :odd and :even are 0 based
-    tbody.find('tr').removeClass('odd').removeClass('even')
-        .filter(':odd').addClass('even').end()
-        .filter(':even').addClass('odd');
-}
